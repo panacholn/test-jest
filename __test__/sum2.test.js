e356abd58aca936cf945a74d66ffec1ce7cd5ab6
Panacholn <panacholn@gmail.com>
2024-09-04 16:13:15 +0700

    sum 2

---
+++ __test__/sum2.test.js
@@ -0,0 +1,5 @@
+const sum2 = require('../sum2');
+
+test('adds 1 + 2 to equal 5', () => {
+  expect(sum2(1, 2)).toBe(5);
+});
---
+++ sum2.js
@@ -0,0 +1,6 @@
+const sum = require('./sum');
+
+function sum2(a, b) {
+  return sum(a, b) + 2;
+}
+module.exports = sum2;
